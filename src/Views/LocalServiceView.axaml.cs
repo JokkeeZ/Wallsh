2a2a957b6c95daa94c15fc4e20395add87b881a7
Jokkeez <11576776+JokkeeZ@users.noreply.github.com>
2025-01-20 18:56:31 +0200

    Refactor wallpaper services into own views

---
+++ src/Views/LocalServiceView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace Wallsh.Views;
+
+public partial class LocalServiceView : UserControl
+{
+    public LocalServiceView() => InitializeComponent();
+}
---
+++ src/Views/WallhavenServiceView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace Wallsh.Views;
+
+public partial class WallhavenServiceView : UserControl
+{
+    public WallhavenServiceView() => InitializeComponent();
+}
